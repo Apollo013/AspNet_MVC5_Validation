39df69aea1f72335970407a5f6286465e440a5ea
Paul <paul.j.millar.1971@gmail.com>
2016-10-27 11:33:26 +0100

    Add project files.

---
+++ AspNet_MVC5_Validation/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AspNet_MVC5_Validation
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AspNet_MVC5_Validation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNet_MVC5_Validation.MvcApplication" Language="C#" %>
